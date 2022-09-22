770572ce375a6a96485490d1d649e5646324ed2a
freddyfernandez <fernandezyc@hotmail.com>
2022-09-22 13:51:50 -0500

    first commit

---
+++ src/main/java/pe/com/nttdata/ProyectoSelenium/ProyectoSeleniumApplication.java
@@ -0,0 +1,13 @@
+package pe.com.nttdata.ProyectoSelenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSeleniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSeleniumApplication.class, args);
+	}
+
+}
